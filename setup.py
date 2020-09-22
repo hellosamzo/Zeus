e14845df52929067a79fe51698cb748710780ca9
Sam <samuelmatthews96@gmail.com>
2020-09-22 21:27:34 +0100

    Created new MyWindow class and added more button functionality

--- setup.py
+++ setup.py
@@ -2,23 +2,33 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def b1_clicked():
-	print("clicked")
+class MyWindow(QMainWindow):
+	def __init__(self):
+		super(MyWindow, self).__init__()
+		self.setGeometry(200, 200, 300, 300)
+		self.setWindowTitle("Zeus")
+		self.initUI()
+	
+	def initUI(self):
+		self.label = QtWidgets.QLabel(self)
+		self.label.setText("Test Label")
+		self.label.move(50,50)
+
+		self.b1 = QtWidgets.QPushButton(self)
+		self.b1.setText("Click Me")
+		self.b1.clicked.connect(self.b1_clicked)
+
+	def b1_clicked(self):
+		self.label.setText("pressed")
+		self.update()
+
+	def update(self):
+		self.label.setStyleSheet("border: 1px solid black;")
+		self.label.adjustSize()
 
 def window():
 	app = QApplication(sys.argv)
-	win = QMainWindow()
-	win.setGeometry(200, 200, 300, 300)
-	win.setWindowTitle("Zeus")
-
-	label = QtWidgets.QLabel(win)
-	label.setText("Test Label")
-	label.move(50,50)
-
-	b1 = QtWidgets.QPushButton(win)
-	b1.setText("Click Me")
-	b1.clicked.connect(b1_clicked)
-
+	win = MyWindow()
 	win.show()
 	sys.exit(app.exec_())
 
